191aa011665fb134d45ab792c317f6f5311da8e8
lidigobeyond <khkh4631@gmail.com>
2025-11-28 22:48:51 +0900

    Revert "chore: operation 스팬 생성 안되는 문제 디버깅"
    This reverts commit 19cb0d77fe2099a3d6a63fb86dbe161dd113ee38.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,11 @@
 /*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
-import { PrismaInstrumentation } from '@prisma/instrumentation';
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
@@ -24,24 +23,7 @@ const sdk = new NodeSDK({
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
-    new GraphQLInstrumentation({
-      // Operation span과 resolver span 모두 생성
-      mergeItems: false,
-      // Trivial resolver (id, name 등) 제외
-      ignoreTrivialResolveSpans: true,
-      // 값을 attribute에 포함하지 않음 (보안)
-      allowValues: false,
-      // 모든 depth 추적
-      depth: -1,
-      // 디버깅용: operation 정보 출력
-      responseHook: (span, data) => {
-        console.log(
-          'GraphQL instrumentation data:',
-          JSON.stringify(data, null, 2),
-        );
-      },
-    }),
-    new PrismaInstrumentation(),
+    new GraphQLInstrumentation(),
   ],
 });
 
